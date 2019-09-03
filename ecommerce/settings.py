586aee04177d68f7a5fe8a872176050f342ca63b
Ubuntu <ubuntu@ip-172-31-31-192.ec2.internal>
2019-09-03 13:22:23 +0000

    test added to test.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'accounts'
+    'accounts',
+    'products'
 ]
 
 MIDDLEWARE = [
